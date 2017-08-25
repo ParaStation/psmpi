c21a210b1236a45dc773373a73666854b90411e5
clauss <clauss@par-tec.com>
2017-08-25 15:18:08 +0200

    Let single processes disjoin the MPI session properly by calling MPI_Abort on MPI_COMM_SELF. Since this is demands for sparse synchronization in MPI_Finalize, this is only enabled when PSP_FINALIZE_BARRIER=0 is set explicitly.

--- mpich2/src/mpid/psp/src/mpid_abort.c
+++ mpich2/src/mpid/psp/src/mpid_abort.c
@@ -12,12 +12,30 @@
 
 #include "mpidimpl.h"
 
+static
+int _getenv_i(const char *env_name, int _default)
+{
+	char *val = getenv(env_name);
+	return val ? atoi(val) : _default;
+}
+
 #define FCNAME "MPID_Abort"
 #define FUNCNAME MPID_Abort
-int MPID_Abort(MPID_Comm * comm, int mpi_errno, int exit_code,
+int MPID_Abort(MPID_Comm * comm_ptr, int mpi_errno, int exit_code,
 	       const char *error_msg)
 {
-	/* printf("ps--- %s() called\n", __func__); */
+	MPID_Comm *comm_self_ptr;
+
+	MPID_Comm_get_ptr(MPI_COMM_SELF, comm_self_ptr);
+
+	if( (comm_ptr == comm_self_ptr) && (!_getenv_i("PSP_FINALIZE_BARRIER", 1)) ) {
+
+		/* Experimental extension:
+		   Properly deregister from PMI in the COMM_SELF case
+		   so that other processes can still carry on.*/
+
+		PMI_Finalize();
+	}
 
 	MPL_error_printf("%s", error_msg);
 
