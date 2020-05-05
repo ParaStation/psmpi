c8883b1f2f1e86a49a3986ad8ebd069d5b5c7e89
Carsten Clauss <clauss@par-tec.com>
2020-05-05 12:04:14 +0200

    hcoll: Add shortcut (and fix deadlock with hcoll_Allreduce) for the zero-byte case. (It seems as if the recent HCOLL version has some general problems with zero-byte messages...)

--- mpich2/src/mpid/psp/include/mpid_coll.h
+++ mpich2/src/mpid/psp/include/mpid_coll.h
@@ -58,6 +58,11 @@ static inline int MPID_Bcast(void *buffer, int count, MPI_Datatype datatype, int
     int mpi_errno = MPI_SUCCESS;
 
 #ifdef HAVE_LIBHCOLL
+    int typesize;
+    MPIR_Datatype_get_size_macro(datatype, typesize);
+    if (count * typesize == 0)
+        goto fn_exit; /* do shortcut here (as it seems that HCOLL has problems with zero-byte messages) */
+
     mpi_errno = hcoll_Bcast(buffer, count, datatype, root, comm, errflag);
     if (mpi_errno == MPI_SUCCESS)
         goto fn_exit;
@@ -92,6 +97,11 @@ static inline int MPID_Allreduce(const void *sendbuf, void *recvbuf, int count,
     int mpi_errno = MPI_SUCCESS;
 
 #ifdef HAVE_LIBHCOLL
+    int typesize;
+    MPIR_Datatype_get_size_macro(datatype, typesize);
+    if (count * typesize == 0)
+        goto fn_exit; /* do shortcut here (as it seems that HCOLL has problems with zero-byte messages) */
+
     mpi_errno = hcoll_Allreduce(sendbuf, recvbuf, count, datatype, op, comm, errflag);
     if (mpi_errno == MPI_SUCCESS)
         goto fn_exit;
@@ -131,6 +141,12 @@ static inline int MPID_Allgather(const void *sendbuf, int sendcount, MPI_Datatyp
     int mpi_errno = MPI_SUCCESS;
 
 #ifdef HAVE_LIBHCOLL
+    int stypesize, rtypesize;
+    MPIR_Datatype_get_size_macro(sendtype, stypesize);
+    MPIR_Datatype_get_size_macro(recvtype, rtypesize);
+    if (((sendcount * stypesize == 0) && (sendbuf != MPI_IN_PLACE)) || (recvcount * rtypesize == 0))
+        goto fn_exit; /* do shortcut here (as it seems that HCOLL has problems with zero-byte messages) */
+
     mpi_errno = hcoll_Allgather(sendbuf, sendcount, sendtype, recvbuf,
                                 recvcount, recvtype, comm, errflag);
     if (mpi_errno == MPI_SUCCESS)
