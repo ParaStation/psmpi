6215deda12d679c93077bec217b3c9ee1ebc1ebd
Carsten Clauss <clauss@par-tec.com>
2021-07-14 14:51:09 +0200

    test/cuda: Rework laplace_mpi/simple_kernel tests
    These tests didn't check for CUDA errors so that such could lead to
    error messages supposedly related to the MPI layer. This commit
    fixes this problem by explicitly checking for CUDA errors and
    treating them as fatal if they would also affect the MPI layer.
    
    In addition, the tests have been revised (also with respect to the
    use of the mtest framework) and checked for algorithmic correctness.

--- mpich2/test/mpi/cuda/cuda_laplace_mpi.cu
+++ mpich2/test/mpi/cuda/cuda_laplace_mpi.cu
@@ -1,7 +1,7 @@
 /*
  * ParaStation
  *
- * Copyright (C) 2016 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2016-2021 ParTec Cluster AG, Munich
  *
  * This file may be distributed under the terms of the Q Public License
  * as defined in the file LICENSE.QPL included in the packaging of this
@@ -11,7 +11,9 @@
  */
 
 #include <mpi.h>
-
+extern "C" {
+#include "mpitest.h"
+}
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,69 +26,81 @@
 #include <driver_types.h>
 #endif
 
-#if 1
-#define printf(...)
-#endif
+#define ThreadsPerBlock 1
 
+// Total domain size:
 #define domain_size_x 256
 #define domain_size_y 256
 
 #ifdef __NVCC__
-__global__ void laplace(float **old_values, float **new_values)
+__global__ void laplace(float *old_values, float *new_values, int n, int m)
 {
-	int i = threadIdx.x + 1;
-	int j = blockIdx.x + 1;
+	int i = threadIdx.x;
+	int j = blockIdx.x;
 
-	new_values[i][j] = 0.25 * (old_values[i-1][j] + old_values[i+1][j] + old_values[i][j-1] + old_values[i][j+1]);
+	if ((i > 0) && (i < n-1) &&
+	    (j > 0) && (j < m-1)) {
+		new_values[i*m + j] = 0.25 *
+			(old_values[(i-1)*m + j] +
+			 old_values[(i+1)*m + j] +
+			 old_values[i*m + j-1]   +
+			 old_values[i*m + j+1]   );
+	}
 }
 
-__global__ void swap_arrays(float **old_values, float **new_values)
+__global__ void swap_arrays(float *old_values, float *new_values, int n, int m)
 {
-	int i = threadIdx.x + 1;
-	int j = blockIdx.x + 1;
+	int i = threadIdx.x;
+	int j = blockIdx.x;
 
-	old_values[i][j] = new_values[i][j];
-}
-
-__global__ void set_pointers(float **lines, float *blob, int n, int m)
-{
-	int i;
-
-	lines[0] = blob;
-
-	for (i = 1; i < n+2; i++) {
-
-		lines[i] = lines[i-1] + (m+2);
+	if ((i > 0) && (i < n-1) &&
+	    (j > 0) && (j < m-1)) {
+		new_values[i*m + j] = old_values[i*m + j];
 	}
 }
 #endif
 
+#define CUDA_CHECK(call, fatal)                                         \
+	do {                                                            \
+		if ((cuda_error == cudaSuccess) && (((call) != cudaSuccess) || ((cuda_error = cudaGetLastError()) != cudaSuccess))) { \
+			MTestPrintfMsg(0, #call" returned: %s\n", cudaGetErrorString(cuda_error)); \
+			if (fatal) MTestError("Fatal CUDA error! Calling MPI_Abort()...\n");			\
+		}                                                       \
+	} while(0);
+
+
+double start_time, stop_time;
+
 int main(int argc, char **argv)
 {
+	int errs = 0;
 	int t, Tmax = 10000;
 
 	int i, j;
 	int my_rank;
 	int num_ranks;
 
-	int n, N = domain_size_x;
-	int m, M = domain_size_y;
+	int n = domain_size_x;
+	int m = domain_size_y;
 
 	double residual = 0.0;
-
-	void* ptr;
 
 	float** new_values;
 	float** old_values;
 	float* new_values_blob;
 	float* old_values_blob;
 
-	float** __new_values;
-	float** __old_values;
+	float* send_buffer_up;
+	float* send_buffer_dn;
+	float* recv_buffer_up;
+	float* recv_buffer_dn;
+
+#ifdef __NVCC__
+	cudaError_t cuda_error = cudaSuccess;
 	float* __new_values_blob;
 	float* __old_values_blob;
-
-	double start_time, stop_time;
+	void* ptr;
+#endif
 
 	MPI_Request* upper_send_req;
 	MPI_Request* lower_send_req;
@@ -103,140 +117,143 @@ int main(int argc, char **argv)
 	lower_recv_req = req_array + 3;
 
 
-	MPI_Init(&argc, &argv);
+	MTest_Init(&argc, &argv);
 
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &num_ranks);
 
+	n = domain_size_x / num_ranks;
+	if (my_rank == num_ranks-1) n += domain_size_x % num_ranks;
 
-	m = M;
-	n = N/num_ranks;
-	if(my_rank == num_ranks-1) n += N%num_ranks;
+	m += 2;
+	n += 2;
 
-	printf("(%d) %d x %d / (%d x %d)\n", my_rank, N, M, n, m);
+	MTestPrintfMsg(3, "(%d) %d x %d / (%d x %d)\n", my_rank, domain_size_x, domain_size_y, n, m);
 
+	new_values = (float**)malloc(n * sizeof(float*));
+	new_values_blob = (float *)malloc(n * m * sizeof(float));
 
-	new_values = (float**)malloc((n+2)*sizeof(float*));
-	new_values_blob = (float *)malloc((n+2)*(m+2)*sizeof(float));
-
-	old_values = (float**)malloc((n+2)*sizeof(float*));
-	old_values_blob = (float *)malloc((n+2)*(m+2)*sizeof(float));
+	old_values = (float**)malloc(n * sizeof(float*));
+	old_values_blob = (float *)malloc(n * m * sizeof(float));
 
 	new_values[0] = new_values_blob;
 	old_values[0] = old_values_blob;
-	for (i = 1; i < n+2; i++) {
+	for (i = 1; i < n; i++) {
 
-		new_values[i] = new_values[i-1] + (m+2);
-		old_values[i] = old_values[i-1] + (m+2);
+		new_values[i] = new_values[i-1] + m;
+		old_values[i] = old_values[i-1] + m;
 	}
 
 #ifdef __NVCC__
-	cudaMalloc((void**)&ptr, (n+2)*sizeof(float*));
-	__new_values = (float**)ptr;
-	cudaMalloc((void**)&ptr, (n+2)*(m+2)*sizeof(float));
+	CUDA_CHECK(cudaMalloc((void**)&ptr, n * m * sizeof(float)), 1);
 	__new_values_blob = (float*)ptr;
-
-	cudaMalloc((void**)&ptr, (n+2)*sizeof(float*));
-	__old_values = (float**)ptr;
-	cudaMalloc((void**)&ptr, (n+2)*(m+2)*sizeof(float));
+	CUDA_CHECK(cudaMalloc((void**)&ptr, n * m * sizeof(float)), 1);
 	__old_values_blob = (float*)ptr;
-
-	set_pointers<<< 1, 1 >>>(__old_values, __old_values_blob, n, m);
-	set_pointers<<< 1, 1 >>>(__new_values, __new_values_blob, n, m);
 #endif
 
-	printf("(%d) Memory allocated!\n", my_rank); fflush(stdout);
+	MTestPrintfMsg(1, "(%d) Memory allocated!\n", my_rank);
 
 
-	for(i=0; i<n+2; i++) {
+	for (i = 0; i < n; i++) {
 
-		for(j=0; j<m+2; j++) {
+		for (j = 0; j < m; j++) {
 
 			new_values[i][j] = 0.0;
 		}
 	}
 
-	if(my_rank == 0) {
+	if (my_rank == 0) {
 
-		for(j=0; j<m+2; j++) {
+		for (j = 0; j < m; j++) {
 
 			new_values[0][j] = 100.0;
 		}
 	}
 
-	if(my_rank == num_ranks-1) {
+	if (my_rank == num_ranks-1) {
 
-		for(j=0; j<m+2; j++) {
+		for (j = 0; j < m; j++) {
 
-			new_values[n+1][j] = 100.0;
+			new_values[n-1][j] = 100.0;
 		}
 	}
 
-	for(i=0; i<n+2; i++) {
+	for (i = 0; i < n; i++) {
 
 		new_values[i][0]   = 100.0;
-		new_values[i][N+1] = 100.0;
+		new_values[i][m-1] = 100.0;
 	}
 
-	for(i=0; i<n+2; i++) {
+	for (i = 0; i < n; i++) {
 
-		for(j=0; j<m+2; j++) {
+		for (j = 0; j < m; j++) {
 
 			old_values[i][j] = new_values[i][j];
 		}
 	}
 
+#ifdef __NVCC__
+	CUDA_CHECK(cudaMemcpy(__new_values_blob, new_values_blob, n * m * sizeof(float), cudaMemcpyHostToDevice), 0);
+	CUDA_CHECK(cudaMemcpy(__old_values_blob, old_values_blob, n * m * sizeof(float), cudaMemcpyHostToDevice), 0);
 	MPI_Barrier(MPI_COMM_WORLD);
 
-#ifdef __NVCC__
-	cudaMemcpy(__new_values_blob, new_values_blob, (n+2)*(m+2)*sizeof(float), cudaMemcpyHostToDevice);
-	cudaMemcpy(__old_values_blob, old_values_blob, (n+2)*(m+2)*sizeof(float), cudaMemcpyHostToDevice);
-	cudaMemcpy(new_values, __new_values, (n+2)*sizeof(float*), cudaMemcpyDeviceToHost);
-	cudaMemcpy(old_values, __old_values, (n+2)*sizeof(float*), cudaMemcpyDeviceToHost);
-	MPI_Barrier(MPI_COMM_WORLD);
+	send_buffer_up = &__new_values_blob[m];
+	send_buffer_dn = &__new_values_blob[(n-2)*m];
+	recv_buffer_up = &__old_values_blob[0];
+	recv_buffer_dn = &__old_values_blob[(n-1)*m];
+
+#else
+	send_buffer_up = new_values[1];
+	send_buffer_dn = new_values[n-2];
+	recv_buffer_up = old_values[0];
+	recv_buffer_dn = old_values[n-1];
 #endif
 
-	printf("(%d) Arrays initialized!\n", my_rank); fflush(stdout);
+	MTestPrintfMsg(1, "(%d) Arrays initialized!\n", my_rank);
 
 	start_time = MPI_Wtime();
 
-	for(t=0; t<=Tmax; t++){
+	for (t = 0; t <= Tmax; t++){
 
 #ifdef __NVCC__
-		laplace<<< m, n >>>(__old_values, __new_values);
+		laplace<<< m, n >>>(__old_values_blob, __new_values_blob, n, m);
+		CUDA_CHECK(cudaDeviceSynchronize(), 0);
 #else
-		for(i=1; i<n+1; i++) {
+		for (i = 1; i < n-1; i++) {
 
-			for(j=1; j<m+1; j++) {
+			for (j = 1; j < m-1; j++) {
 
 				new_values[i][j] = 0.25 * ( old_values[i-1][j] + old_values[i+1][j] +
 							    old_values[i][j-1] + old_values[i][j+1] );
 			}
 		}
 #endif
-		if(my_rank != 0) {
 
-			MPI_Isend(new_values[1], m+2, MPI_FLOAT, my_rank-1, 0, MPI_COMM_WORLD, upper_send_req);
-			MPI_Irecv(old_values[0], m+2, MPI_FLOAT, my_rank-1, 0, MPI_COMM_WORLD, upper_recv_req);
+		if (my_rank != 0) {
+
+			MPI_Isend(send_buffer_up, m, MPI_FLOAT, my_rank-1, 0, MPI_COMM_WORLD, upper_send_req);
+			MPI_Irecv(recv_buffer_up, m, MPI_FLOAT, my_rank-1, 0, MPI_COMM_WORLD, upper_recv_req);
 		}
 
-		if(my_rank != num_ranks-1) {
+		if (my_rank != num_ranks-1) {
 
-			MPI_Irecv(old_values[n+1], m+2, MPI_FLOAT, my_rank+1, 0, MPI_COMM_WORLD, lower_recv_req);
-			MPI_Isend(new_values[n],   m+2, MPI_FLOAT, my_rank+1, 0, MPI_COMM_WORLD, lower_send_req);
+			MPI_Irecv(recv_buffer_dn, m, MPI_FLOAT, my_rank+1, 0, MPI_COMM_WORLD, lower_recv_req);
+			MPI_Isend(send_buffer_dn, m, MPI_FLOAT, my_rank+1, 0, MPI_COMM_WORLD, lower_send_req);
 		}
 
 		MPI_Waitall(4, req_array, stat_array);
 
-		if(t<Tmax) {
+		if (t < Tmax) {
+
 #ifdef __NVCC__
-			swap_arrays<<< m, n >>>(__old_values, __new_values);
+			swap_arrays<<< m, n >>>(__new_values_blob, __old_values_blob, n, m);
+			CUDA_CHECK(cudaDeviceSynchronize(), 0);
 #else
-			for(i=1; i<n+1; i++) {
+			for (i = 1; i < n-1; i++) {
 
-				for(j=1; j<m+1; j++) {
+				for (j = 1; j < m-1; j++) {
 
-					old_values[i][j]=new_values[i][j];
+					old_values[i][j] = new_values[i][j];
 				}
 			}
 #endif
@@ -246,23 +263,14 @@ int main(int argc, char **argv)
 	MPI_Barrier(MPI_COMM_WORLD);
 
 #ifdef __NVCC__
-	cudaMemcpy(new_values_blob, __new_values_blob, (n+2)*(m+2)*sizeof(float), cudaMemcpyDeviceToHost);
-	cudaMemcpy(old_values_blob, __old_values_blob, (n+2)*(m+2)*sizeof(float), cudaMemcpyDeviceToHost);
-
-	new_values[0] = new_values_blob;
-	old_values[0] = old_values_blob;
-	for (i = 1; i < n+2; i++) {
-
-		new_values[i] = new_values[i-1] + (m+2);
-		old_values[i] = old_values[i-1] + (m+2);
-	}
-
+	CUDA_CHECK(cudaMemcpy(new_values_blob, __new_values_blob, n * m * sizeof(float), cudaMemcpyDeviceToHost), 1);
+	CUDA_CHECK(cudaMemcpy(old_values_blob, __old_values_blob, n * m * sizeof(float), cudaMemcpyDeviceToHost), 1);
 	MPI_Barrier(MPI_COMM_WORLD);
 #endif
 
-	for(i=1; i<n+1; i++) {
+	for (i = 1; i < n-1; i++) {
 
-		for(j=1; j<m+1; j++) {
+		for (j = 1; j < m-1; j++) {
 
 			residual += fabs(old_values[i][j] - new_values[i][j]);
 		}
@@ -274,7 +282,8 @@ int main(int argc, char **argv)
 
 	stop_time = MPI_Wtime();
 
-	printf("(%d) Algorithm completed! Residual = %f / Needed time: %f\n", my_rank, residual, stop_time - start_time); fflush(stdout);
+	MTestPrintfMsg(1, "(%d) Algorithm completed!\n", my_rank);
+	MTestPrintfMsg(2, "(%d) Residual = %f / Needed time: %f\n", my_rank, residual, stop_time - start_time);
 
 	free(new_values);
 	free(new_values_blob);
@@ -282,17 +291,10 @@ int main(int argc, char **argv)
 	free(old_values_blob);
 
 #ifdef __NVCC__
-	cudaFree(__new_values);
-	cudaFree(__new_values_blob);
-	cudaFree(__old_values);
-	cudaFree(__old_values_blob);
+	CUDA_CHECK(cudaFree(__new_values_blob), 0);
+	CUDA_CHECK(cudaFree(__old_values_blob), 0);
 #endif
 
-	MPI_Finalize();
-
-	if(my_rank == 0) {
-		fprintf(stdout, "No Errors\n"); fflush(stdout);
-	}
-
-	return 0;
+	MTest_Finalize(errs);
+	return MTestReturnValue(errs);
 }
