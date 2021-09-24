1fb1e763f599c29e87816176777b78bbc0bf608a
Carsten Clauss <clauss@par-tec.com>
2021-09-24 14:18:20 +0200

    vendor/coll: Fix memory leak for large messages
    This fixes a memory leak in MPIR_Igather_intra_sched_binomial() for
    a temporary datatype created here in case of very large messages.

--- mpich2/src/mpi/coll/igather/igather_intra_sched_binomial.c
+++ mpich2/src/mpi/coll/igather/igather_intra_sched_binomial.c
@@ -209,6 +209,8 @@ int MPIR_Igather_intra_sched_binomial(const void *sendbuf, int sendcount, MPI_Da
                 if (curr_cnt - nbytes != (int) (curr_cnt - nbytes)) {
                     blocks[1] = 1;
                     MPIR_Type_contiguous_x_impl(curr_cnt - nbytes, MPI_BYTE, &(types[1]));
+                    mpi_errno = MPIR_Type_commit_impl(&(types[1]));
+                    MPIR_ERR_CHECK(mpi_errno);
                 } else {
                     MPIR_Assign_trunc(blocks[1], curr_cnt - nbytes, int);
                     types[1] = MPI_BYTE;
@@ -227,6 +229,8 @@ int MPIR_Igather_intra_sched_binomial(const void *sendbuf, int sendcount, MPI_Da
 
                 /* this "premature" free is safe b/c the sched holds an actual ref to keep it alive */
                 MPIR_Type_free_impl(&tmp_type);
+                if (types[1] != MPI_BYTE)
+                        MPIR_Type_free_impl(&(types[1]));
             }
 
             break;
