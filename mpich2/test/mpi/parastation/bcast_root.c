2f530b4a40c28653e788a77fdcb4e92b382c7730
Carsten Clauss <clauss@par-tec.com>
2022-08-18 15:08:40 +0200

    test: Fix and revise (i)bcast_root tests
    It is not a good idea to rely on `rand()` always returning the same
    sequence on all processes -- as has been done for the message length
    in these two parastation tests before. This commit fixes that and also
    cleans up a bit the test codes.

--- mpich2/test/mpi/parastation/bcast_root.c
+++ mpich2/test/mpi/parastation/bcast_root.c
@@ -13,66 +13,67 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
+#include "mpitest.h"
 
-/* This test rotates in a loop through the root rank
- * for Bcast() with a total of np operations.
+/* This test iterates in a loop with increasing message sizes
+ * over all possible root ranks for Bcast() operations.
  */
 
 /* 12288 = MPIR_CVAR_BCAST_SHORT_MSG_SIZE */
-#define MAX_MSGLEN 2 * 12288 + 10
+#define MAX_MSGLEN 2 * 12288
 #define MAX_PROCS 256
 
 char buf[MAX_PROCS][MAX_MSGLEN];
 
+#define MIN(X,Y) ((X < Y) ? (X) : (Y))
+
 int main(int argc, char* argv[])
 {
+	unsigned errs = 0;
 	int i, j;
 	int msglen;
 	int comm_rank;
 	int comm_size;
 	MPI_Comm comm;
 
-	MPI_Init(&argc, &argv);
+	MTest_Init(&argc, &argv);
 
 	comm = MPI_COMM_WORLD;
 
 	MPI_Comm_size(comm, &comm_size);
 	MPI_Comm_rank(comm, &comm_rank);
 
-	if(comm_size > MAX_PROCS) {
+	if (comm_size > MAX_PROCS) {
 		printf("This program can handle up to np = %d (vs. %d started) processes! Calling MPI_Abort()...\n", MAX_PROCS, comm_size);
 		MPI_Abort(MPI_COMM_WORLD, -1);
 	}
 
+	/* ensure to test with messages before/after the MPIR_CVAR_BCAST_SHORT_MSG_SIZE threshold (= MAX_MSGLEN / 2), see above */
+	for (msglen = 1; msglen <= MAX_MSGLEN; msglen = MIN(MAX_MSGLEN, msglen * 2) + (msglen == MAX_MSGLEN)) {
 
-	for(msglen = 1; msglen < MAX_MSGLEN; msglen = msglen*2 + rand() % 10) {
 		for (i = 0; i < comm_size; ++i) {
-			for(j = 0; j < msglen; j++) {
+			for (j = 0; j < msglen; j++) {
 				if (comm_rank == i) {
 					buf[i][j] = comm_rank;
 				} else {
 					buf[i][j] = -1;
 				}
-			}			
+			}
 			MPI_Bcast(buf[i], msglen, MPI_BYTE, i, comm);
 		}
 
 		for (i = 0; i < comm_size; ++i) {
-			for(j = 0; j < msglen; j++) {
-				if(buf[i][j] != i) {
-					printf("(%d) ERROR: got %d but expected %d at index %d\n", comm_rank, buf[i][j], i, j);
-					MPI_Abort(MPI_COMM_WORLD, -1);
+			for (j = 0; j < msglen; j++) {
+				if (buf[i][j] != i) {
+					if (errs < 10) fprintf(stderr, "(%d) ERROR: got %d but expected %d at index %d\n", comm_rank, buf[i][j], i, j);
+					errs++;
 				}
 				buf[i][j] = -1;
 			}
 		}
 	}
 
-	if(comm_rank == 0) {
-		printf(" No errors\n");
-	}
-
-	MPI_Finalize();
+	MTest_Finalize(errs);
 
 	return 0;
 }
