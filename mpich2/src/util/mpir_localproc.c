8cb23c5028cdef1393c0bf48bd8bf86735fbd364
Carsten Clauss <clauss@par-tec.com>
2022-11-03 17:30:16 +0100

    vendor/msa: Adaptation to renaming from "toplogy" to "msa" awarenes.

--- mpich2/src/util/mpir_localproc.c
+++ mpich2/src/util/mpir_localproc.c
@@ -53,7 +53,7 @@ int MPIR_Find_local(MPIR_Comm * comm, int *local_size_p, int *local_rank_p,
     for (i = 0; i < comm->remote_size; ++i)
         intranode_table[i] = -1;
 
-#ifndef MPID_PSP_TOPOLOGY_AWARE_COLLOPS
+#ifndef MPID_PSP_MSA_AWARE_COLLOPS
     mpi_errno = MPID_Get_node_id(comm, comm->rank, &my_node_id);
 #else
     mpi_errno = MPID_Get_badge(comm, comm->rank, &my_node_id);
@@ -66,7 +66,7 @@ int MPIR_Find_local(MPIR_Comm * comm, int *local_size_p, int *local_rank_p,
 
     /* Scan through the list of processes in comm. */
     for (i = 0; i < comm->remote_size; ++i) {
-#ifndef MPID_PSP_TOPOLOGY_AWARE_COLLOPS
+#ifndef MPID_PSP_MSA_AWARE_COLLOPS
         mpi_errno = MPID_Get_node_id(comm, i, &node_id);
 #else
         mpi_errno = MPID_Get_badge(comm, i, &node_id);
@@ -168,7 +168,7 @@ int MPIR_Find_external(MPIR_Comm * comm, int *external_size_p, int *external_ran
     MPIR_CHKPMEM_MALLOC(internode_table, int *, sizeof(int) * comm->remote_size, mpi_errno,
                         "internode_table", MPL_MEM_COMM);
 
-#ifndef MPID_PSP_TOPOLOGY_AWARE_COLLOPS
+#ifndef MPID_PSP_MSA_AWARE_COLLOPS
     int num_nodes = MPIR_Process.num_nodes;
 #else
     int num_nodes;
@@ -185,7 +185,7 @@ int MPIR_Find_external(MPIR_Comm * comm, int *external_size_p, int *external_ran
     external_rank = -1;
 
     for (i = 0; i < comm->remote_size; ++i) {
-#ifndef MPID_PSP_TOPOLOGY_AWARE_COLLOPS
+#ifndef MPID_PSP_MSA_AWARE_COLLOPS
         mpi_errno = MPID_Get_node_id(comm, i, &node_id);
 #else
         mpi_errno = MPID_Get_badge(comm, i, &node_id);
