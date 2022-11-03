7d349be40abd53b591ac85bca5f5679ba347d9bb
Carsten Clauss <clauss@par-tec.com>
2022-11-03 15:46:39 +0100

    test: Remove 8k/16k test for the number of communicators.
    Now that we no longer set `MPIR_CONTEXT_SUBCOMM_WIDTH` to zero in the
    default case, we must also no longer use the tests `num_contexts_8k`
    and `num_contexts_16k` because the number of communicators should now
    be limited by 4k.
    
    (...which is still larger by a factor of two than with vanilla MPICH
    sinc we continue to set `MPIR_CONTEXT_DYNAMIC_PROC_WIDTH=0`.)

--- mpich2/test/mpi/parastation/num_contexts_4k.c
+++ mpich2/test/mpi/parastation/num_contexts_4k.c
@@ -15,16 +15,17 @@
 /* Check for 4k custom/dynamic communicators: (minus COMM_SELF, COMM_WORLD, and ICOMM_WORLD if needed) */
 #define NUM_COMMS (4 * 1024 - 3)
 
-/* If this test fails, then try to set MPIR_CONTEXT_DYNAMIC_PROC_WIDTH to (0) in mpich2/src/include/mpiimpl.h */
+/* If this test fails, then try to set MPIR_CONTEXT_DYNAMIC_PROC_WIDTH to (0) in mpir_contextid.h */
 
 int main(int argc, char* argv[])
 {
+	unsigned errs = 0;
 	int i;
 	int world_rank, rank[NUM_COMMS];
 	int world_size, size[NUM_COMMS];
 	MPI_Comm comm_array[NUM_COMMS];
 
-	MPI_Init(&argc, &argv);
+	MTest_Init(&argc, &argv);
 
 	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
@@ -39,11 +40,12 @@ int main(int argc, char* argv[])
 		rc = MPI_Comm_split(MPI_COMM_WORLD, color, world_rank, &comm_array[i]);
 
 		if(rc != MPI_SUCCESS) {
-			printf("\nThe maximum number of custom/dynamic communicators/contexts is %d but this test checks for %d.\n", i, NUM_COMMS);
-			printf("Try to set MPIR_CONTEXT_DYNAMIC_PROC to (0) in mpich2/src/include/mpiimpl.h to get more contexts.\n");
-			printf("This test is known to fail with CH3 device.\n\n");
-			MPI_Errhandler_set(MPI_COMM_WORLD, MPI_ERRORS_ARE_FATAL);
-			MPI_Comm_split(MPI_COMM_WORLD, color, world_rank, &comm_array[i]);
+			if (!errs) {
+				fprintf(stderr, "\nERROR: The maximum number of custom/dynamic communicators/contexts is %d but this test checks for %d.\n", i, NUM_COMMS);
+				fprintf(stderr, "Try to set MPIR_CONTEXT_DYNAMIC_PROC to (0) in mpich2/src/include/mpir_contextid.h to get more contexts.\n");
+				fprintf(stderr, "This test is known to fail with CH3 device.\n\n");
+			}
+			errs++;
 		}
 
 		MPI_Comm_rank(comm_array[i], &rank[i]);
@@ -54,7 +56,7 @@ int main(int argc, char* argv[])
 		MPI_Comm_free(&comm_array[i]);
 	}
 
-	printf(" No errors\n");
+	MTest_Finalize(errs);
 
-	MPI_Finalize();
+	return MTestReturnValue(errs);
 }
