a79f6cad417d002661ea2c671e06bcb3bbc7fcb0
Sonja Happ <sonja.happ@par-tec.com>
2023-07-21 09:44:28 +0200

    vendor/hydra: Remove PMI_KEY_CHECK, replace by debug msg.
    Instead of the config option PMI_KEY_CHECK we rely on the
    global debug setting of hydra to print a debug message if
    duplicate keys are detected.

--- mpich2/src/pm/hydra/proxy/pmip_pmi.c
+++ mpich2/src/pm/hydra/proxy/pmip_pmi.c
@@ -613,7 +613,7 @@ HYD_status fn_finalize(struct pmip_downstream *p, struct PMIU_cmd *pmi)
 HYD_status fn_info_putnodeattr(struct pmip_downstream *p, struct PMIU_cmd *pmi)
 {
     HYD_status status = HYD_SUCCESS;
-    int ret, pmi_errno;
+    int pmi_errno;
     struct PMIU_cmd pmi_response;
     HYDU_FUNC_ENTER();
 
@@ -632,7 +632,7 @@ HYD_status fn_info_putnodeattr(struct pmip_downstream *p, struct PMIU_cmd *pmi)
         goto fn_exit;
     }
 
-    status = HYD_pmcd_pmi_add_kvs(key, val, p->pg->kvs, &ret);
+    status = HYD_pmcd_pmi_add_kvs(key, val, p->pg->kvs, HYD_pmcd_pmip.user_global.debug);
     HYDU_ERR_POP(status, "unable to put data into kvs\n");
 
     pmi_errno = PMIU_msg_set_response(pmi, &pmi_response, is_static);
