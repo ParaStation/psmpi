8f8db18691fd4be583465c2a8265b2b4a8181eb9
Simon Pickartz <pickartz@par-tec.com>
2024-10-26 21:36:52 +0200

    context_id: Adapt to consolidated context offset macros.
    The intra and inter distinctions have been removed in upstream MPICH
    with 7328b9295. This patch adds according adaptations to the PSP device.

--- mpich2/src/mpid/psp/src/mpid_part.c
+++ mpich2/src/mpid/psp/src/mpid_part.c
@@ -573,7 +573,7 @@ int MPID_PSP_part_init_common(const void *buf, int partitions, MPI_Count count,
     preq->tag = tag;
     preq->context_id = comm->context_id;
     preq->info = info;
-    preq->context_offset = MPIR_CONTEXT_INTRA_PT2PT;
+    preq->context_offset = 0;
 
     req->u.part.partitions = partitions;
     MPIR_Part_request_inactivate(req);
